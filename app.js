a5c0222fae62ec236213a4b1c5e33319675b9caa
unknown <yatesgregory92@gmail.com>
2019-02-27 01:03:22 +0000

    added 2 way clicks for icons, so you can now get info when clicking on the map, added the start of the country count aim-going to show the number rather than the icons, also fixed the layering issue so i can now start to think about adding fancy stuff, in desperate need for styling overhaul, and more thoughts on filters

--- app.js
+++ app.js
@@ -29,16 +29,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Create connection
 const conn = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: 'root',
-database: 'test1'
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'test1'
 });
 
 //connect to database
-conn.connect((err) =>{
-if(err) throw err;
-console.log('Mysql Connected...');
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('Mysql Connected...');
 });
 
 var geo = require('mapbox-geocoding');
@@ -50,18 +50,19 @@ geo.setAccessToken('pk.eyJ1IjoiZ3JlZzE5OTIyIiwiYSI6ImNqcGs1MzFkYTAzMWozcHQ2d3U2d
 app.get('/', (req, res) => {
 
   response = {
-    value : req.query.value
+    value: req.query.value
   }
 
-  let sql = "SELECT c.Organisation_Name,c.Country_Name,p.Person_Name, o.Output_Title_Name, a.Author_Names FROM output ao INNER JOIN outputlist o ON o.Output_ID = ao.Output_ID INNER JOIN output_author_country c ON ao.country_fk = c.Output_Author_ID INNER JOIN authors a ON ao.a_fk = a.Author_ID INNER JOIN person p ON ao.p_fk = p.Person_ID";
+  // let sql = "SELECT c.Organisation_Name,c.Country_Name,p.Person_Name, o.Output_Title_Name, a.Author_Names FROM output ao INNER JOIN outputlist o ON o.Output_ID = ao.Output_ID INNER JOIN output_author_country c ON ao.country_fk = c.Output_Author_ID INNER JOIN authors a ON ao.a_fk = a.Author_ID INNER JOIN person p ON ao.p_fk = p.Person_ID";
 
+  let sql = "SELECT o.Organisation_Name, o.Country_Name, a.Output_Title_Name FROM output_author_country o INNER JOIN outputlist a ON o.Output_ID_fk = a.Output_ID LIMIT 10"
   let query = conn.query(sql, (err, results) => {
 
     if (err) throw err;
 
-    
+
     const geoPromise = param => new Promise((resolve, reject) => {
-      geo.geocode('mapbox.places', param, function(err, geoData) {
+      geo.geocode('mapbox.places', param, function (err, geoData) {
         if (err) return reject(err);
         if (geoData) {
           resolve(geoData.features[0])
@@ -72,29 +73,55 @@ app.get('/', (req, res) => {
     });
 
     const promises = results.map(result =>
-    
+
       Promise.all([
         geoPromise(result.Country_Name),
         geoPromise(result.Organisation_Name),
         result.Output_Title_Name
       ])
-   
-      );
 
-    
+    );
 
-      Promise.all(promises)
+
+
+    Promise.all(promises)
       .then((values) => {
         let results = values.map(elmt => elmt[0]);
+   
         let businesses = values.map(elmt => elmt[1]);
-    
+
+
         let names = values.map(elmt => elmt[2]);
+
+        var resultCount = results.filter((i) => (i.place_name === i.place_name).length)
+  
+
+        console.log(JSON.stringify(results))
+
+      //   function mapToProp(data, prop) {
+      //   return data
+      //     .reduce((res, item) => Object
+      //       .assign(res, {
+      //         [item[prop]]: 1 + (res[item[prop]] || 0)
+      //       }), Object.create(null))
+      //   ;
+      // }
+
+  
+
+      // resultCount = mapToProp(results, 'place_name')
+
+     
+
+
         res.render('layouts/layout', {
           results: JSON.stringify(results),
           businesses: JSON.stringify(businesses),
-          names: JSON.stringify(names)
+          names: JSON.stringify(names),
+          resultCount : JSON.stringify(resultCount)
+
         });
-    })
+      })
 
 
 
@@ -103,22 +130,22 @@ app.get('/', (req, res) => {
   });
 });
 
-app.post('/', function(req, res){
+app.post('/', function (req, res) {
   response = {
-    value : req.body.value
+    value: req.body.value
   }
 
-  if(response.value === 'Places'){
+  if (response.value === 'Places') {
 
     let sql = "SELECT c.Organisation_Name,c.Country_Name,p.Person_Name, o.Output_Title_Name, a.Author_Names FROM output ao INNER JOIN outputlist o ON o.Output_ID = ao.Output_ID INNER JOIN output_author_country c ON ao.country_fk = c.Output_Author_ID INNER JOIN authors a ON ao.a_fk = a.Author_ID INNER JOIN person p ON ao.p_fk = p.Person_ID";
 
     let query = conn.query(sql, (err, results) => {
-  
+
       if (err) throw err;
-  
-      
+
+
       const geoPromise = param => new Promise((resolve, reject) => {
-        geo.geocode('mapbox.places', param, function(err, geoData) {
+        geo.geocode('mapbox.places', param, function (err, geoData) {
           if (err) return reject(err);
           if (geoData) {
             resolve(geoData.features[0])
@@ -127,37 +154,37 @@ app.post('/', function(req, res){
           }
         });
       });
-  
+
       const promises = results.map(result =>
-      
+
         Promise.all([
-         
+
           geoPromise(result.Organisation_Name),
-          
+
         ])
-     
-        );
-  
-      
-  
-        Promise.all(promises)
+
+      );
+
+
+
+      Promise.all(promises)
         .then((values) => {
           let businesses = values.map(elmt => elmt[0]);
-      
-          
+
+
           res.render('layouts/business', {
-          //need to also send paper names, otherwise what's the point
+            //need to also send paper names, otherwise what's the point
             businesses: JSON.stringify(businesses)
           });
-       
-    
-    
-    
-      
-    
-    
-      });
-    
+
+
+
+
+
+
+
+        });
+
     });
   }
 });
@@ -169,7 +196,7 @@ conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -180,7 +207,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -191,7 +218,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -203,7 +230,7 @@ app.use(function(err, req, res, next) {
 var port = 1234;
 
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+  console.log('Server is up and running on port numner ' + port);
 });
 
 
