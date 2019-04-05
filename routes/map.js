0f15725bb4e8f4eebfa3500f9ec7445d25045c26
unknown <yatesgregory92@gmail.com>
2019-04-05 10:08:18 +0100

    updating the styles

--- routes/map.js
+++ routes/map.js
@@ -348,7 +348,7 @@ if (XFRAME_WHITELIST.indexOf(req.query.domain) !== -1) {
 
 
     // let sql = "SELECT o.Organisation_Name, o.Country_Name,a.Output_Title_Name,o.Output_Author_Name AS author_names FROM output_author_country o INNER JOIN outputlist a ON o.Output_ID_fk = a.Output_ID LIMIT 20;"
-    let sql = "SELECT DISTINCT l.Organisation_Address, l.Country_Name,a.Output_Title_Name, o.Output_Author_Name AS author_names, l.Organisation_Name,a.Handle FROM test o INNER JOIN outputlist a ON o.Output_ID = a.Output_ID INNER JOIN organisations l ON o.Organisation_ID = l.ID LIMIT 50"
+    let sql = "SELECT DISTINCT l.Organisation_Address, l.Country_Name,a.Output_Title_Name, o.Output_Author_Name AS author_names, l.Organisation_Name,a.Handle FROM test o INNER JOIN outputlist a ON o.Output_ID = a.Output_ID INNER JOIN organisations l ON o.Organisation_ID = l.ID LIMIT 250"
     // let sql = "SELECT distinct a.Organisation_Name, a.Country_Name,o.Output_Title_Name, a.Output_Author_Name as author_names from complete_holding_table a INNER JOIN outputlist o ON a.Paper_ID = o.Output_ID";
 
 
