422b9e7cb813035eca3989e4bde5e89718ff8376
unknown <yatesgregory92@gmail.com>
2019-04-23 09:57:24 +0100

    forgot db.js

--- routes/db.js
+++ routes/db.js
@@ -1,18 +1,18 @@
 var mysql = require('mysql');
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'test1'
-});
-
-//heroku credentials
 // const conn = mysql.createConnection({
-//   host: 'us-cdbr-iron-east-03.cleardb.net',
-//   user: 'ba3356f747f2ab',
-//   password: '3e276e89',
-//   database: 'heroku_fd006092667f923'
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'test1'
 // });
+
+// heroku credentials
+const conn = mysql.createConnection({
+  host: 'us-cdbr-iron-east-03.cleardb.net',
+  user: 'ba3356f747f2ab',
+  password: '3e276e89',
+  database: 'heroku_fd006092667f923'
+});
 
 conn.connect(function(err) {
     if (err) throw err;
